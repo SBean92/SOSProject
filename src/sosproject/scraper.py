3912c4bc568857398d995dc6bbdde5e0dc92c1cf
SBean92 <sean.behan@ucdconnect.ie>
2018-03-20 11:36:03 +0000

    Added a way to delete existing csv

--- src/sosproject/scraper.py
+++ src/sosproject/scraper.py
@@ -8,9 +8,14 @@ Practice for learning to access API
 from jsonschema.validators import requests
 import csv
 import time
+import os
 
 def main():
     while True:
+        try:
+            os.remove('bikes.csv')
+        except OSError:
+            pass
         response = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=cf7b17e2c126f2e1998430272919516c5acb5538")
         data = response.json()
         print(response.status_code)
@@ -29,6 +34,7 @@ def main():
                 bike_writer.writerow(mylist)
         
         time.sleep(120)
+
 
 if __name__ == "__main__":
     main()
