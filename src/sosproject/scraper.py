182b6efee533c1e8e1453230abda555c4d3e1592
SBean92 <sean.behan@ucdconnect.ie>
2018-03-29 13:46:36 +0100

    Updated scraper to split staic and dynamic values. Added sql funcs

--- src/sosproject/scraper.py
+++ src/sosproject/scraper.py
@@ -16,6 +16,7 @@ from collections import OrderedDict
 def scraper():
     try:
         os.remove('bikes.csv')
+        os.remove('static.csv')
     except OSError:
         pass
     
@@ -23,46 +24,109 @@ def scraper():
     data = response.json(object_pairs_hook=OrderedDict)
 
     for i in data:
-        mylist=[]
+        myliststatic=[]
+        mylistdynamic=[]
         for key, value in i.items():
-            if key == "position":
+            if key == "number":
+                myliststatic.append(value)
+                mylistdynamic.append(value)
+            elif key == "position":
                 for new_key, new_value in value.items():
-                    mylist.append(new_value)
+                    myliststatic.append(new_value)
+            elif key == "name" or key == "address" or key == "contract_name":
+                myliststatic.append(value)
             else:
-                mylist.append(value)
+                mylistdynamic.append(value)
         
-
         with open(r"bikes.csv", 'a') as csvfile:
-            bike_writer = csv.writer(csvfile, lineterminator = '\n')
-            bike_writer.writerow(mylist)
+                bike_writer = csv.writer(csvfile, lineterminator = '\n')
+                bike_writer.writerow(mylistdynamic)
 
-def sqlWrite():
-    lines = 0
-    conn = MySQLdb.connect(host = 'sos-database.cvwfzmigbgkv.us-west-2.rds.amazonaws.com', user = 'sos', passwd = 'ozflanagan1', db = 'sosdatabase')
-    cursor = conn.cursor()
+        with open(r"static.csv", 'a') as csvfile:
+                static_writer = csv.writer(csvfile, lineterminator = '\n')
+                static_writer.writerow(myliststatic)
+
+def stationCount():
+    count = 0
     csv_data = csv.reader(open('bikes.csv', 'r'))
     for row in csv_data:
-        lines += 1
-        cursor.execute('INSERT INTO bike(number, street, address, lat, lng, banking, bonus, status, contract, stands, a_stands, a_bikes, timestamp)' +
-                         'VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', row)
-    conn.commit()
+        count += 1
+    return count
+
+def createTable(table):
+    conn = MySQLdb.connect(host = 'sos-database.cvwfzmigbgkv.us-west-2.rds.amazonaws.com', user = 'sos', passwd = 'ozflanagan1', db = 'sosdatabase')
+    cursor = conn.cursor()    
+    if table == 'bike':
+        cursor.execute('CREATE TABLE bike (number int(20) NOT NULL,street varchar(50),address varchar(50),lat float,lng float,banking varchar(5),bonus varchar(5),status varchar(10),contract varchar(10),stands int(3),a_stands int(3),a_bikes int(3),timestamp bigint(20) NOT NULL,CONSTRAINT PK_bike PRIMARY KEY (number,timestamp))')
+        conn.commit()
+    elif table == 'static':
+        cursor.execute('CREATE TABLE bike_static(number int(4) NOT NULL, contract varchar(10), street varchar(50), address varchar(50), lat float, lng float, PRIMARY KEY (number),FOREIGN KEY (number) REFERENCES bike(number))')
+        conn.commit()
     cursor.close()
-    print("Inserting",lines,"lines into table")
+
+def tableExist(table):
+    conn = MySQLdb.connect(host = 'sos-database.cvwfzmigbgkv.us-west-2.rds.amazonaws.com', user = 'sos', passwd = 'ozflanagan1', db = 'sosdatabase')
+    cursor = conn.cursor()
+    cursor.execute("SHOW TABLES LIKE'"+table+"'")
+    if cursor.fetchone():
+        cursor.close()
+        return True
+    else:
+        cursor.close()
+        return False
+
+def sqlWrite(table):
+    conn = MySQLdb.connect(host = 'sos-database.cvwfzmigbgkv.us-west-2.rds.amazonaws.com', user = 'sos', passwd = 'ozflanagan1', db = 'sosdatabase')
+    cursor = conn.cursor()
+    if table == 'bike':
+        lines = 0
+        csv_data = csv.reader(open('bikes.csv', 'r'))
+        for row in csv_data:
+            lines += 1
+            cursor.execute('INSERT INTO bike(number, banking, bonus, status, stands, a_stands, a_bikes, timestamp)' +
+                            'VALUES(%s, %s, %s, %s, %s, %s, %s, %s)', row)
+        conn.commit()
+        print("Inserting",lines,"lines into dynamic table")
+    if table == 'static':
+        lines = 0
+        csv_data = csv.reader(open('static.csv', 'r'))
+        for row in csv_data:
+            lines += 1
+            cursor.execute('INSERT INTO bike_static(number, street, address, lat, lng, contract)' +
+                            'VALUES(%s, %s, %s, %s, %s, %s)', row)
+        conn.commit()
+        print("Inserting",lines,"lines into static table")     
+    cursor.close()    
 
 def main():
-
+    oldMaxStations = 0
     while True:
+        if tableExist("bike"):
+            pass
+        else:
+            createTable("bike")
+        if tableExist("bike_static"):
+            pass
+        else:
+            createTable("static")
         try:
             scraper()
         except Exception:
             print("Error in getting data")
             traceback.print_exc()
-            
         try:
-            sqlWrite()
+            sqlWrite("bike")
         except Exception:
-            print("Error in inserting data")
+            print("Error in inserting data into bike")
             traceback.print_exc()
+        newMaxStations = stationCount()
+        if oldMaxStations != newMaxStations:
+            try:
+                sqlWrite("static")
+            except Exception:
+                print("Error in inserting data into static")
+                traceback.print_exc()
+            oldMaxStations = newMaxStations
         time.sleep(600)
 
 if __name__ == "__main__":
